83d90c440820b3d850d83cadb78b3a2b61d5b8bb
Gakaj <samgedge8@gmail.com>
2026-04-21 20:18:19 +0100

    Added user input validation(fixed bug)

--- PasswordGenerator/password-generator.py
+++ PasswordGenerator/password-generator.py
@@ -3,7 +3,7 @@ print("Auto length is 10 characters")
 
 
 
-punct = "*&^%$£@;:~#]{]}¬`!?<>\/_" #Set of punctuation that can be generated.
+punct = "*&^%$\£@;:~#]{]}¬`!?</>_" #Set of punctuation that can be generated.
 chars = string.ascii_letters + punct # Set of possible characters (letters(a-Z) and punctuation)
 
 def generator_without_length(chars):
@@ -11,11 +11,21 @@ def generator_without_length(chars):
     return password
 
 
-passwordWL = generator_without_length(chars)
 
-while True:
-    try:
-        lengthChoice = bool(input("Generate with length input: True/False: "))
-    except ValueError:
-        print("Invalid Input, try again")
 
+while True: # Validation
+    user_input = input("\rGenerate with length, yes or no: ").strip().upper()
+
+    if user_input == "YES":
+        length_choice = True
+        break
+    elif user_input == "NO":
+        length_choice = False
+        break
+    else:
+        print("Invalid input. Please enter YES or NO.")
+
+
+if length_choice:
+    passwordWL = generator_without_length(chars)
+    print(passwordWL)
